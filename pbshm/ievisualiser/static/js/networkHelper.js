553ecc7c238b428078a8ff8cda93ea434997481c
Josie McCulloch <j.mcculloch@sheffield.ac.uk>
2024-01-23 13:49:38 +0000

    Updated network drawing method

--- pbshm/ievisualiser/static/js/networkHelper.js
+++ pbshm/ievisualiser/static/js/networkHelper.js
@@ -59,7 +59,7 @@ function plotNetworkFromFile(rawtext){
     }
     // If none were given then calculate where to position the nodes.
     if (totalCoords == 0){
-        nodeCoords = positionNodes(edges);
+        nodeCoords = fruchterman_reingold(edges);
         drawNetwork(nodeCoords, edges, elTypes, elNames, true)
     }
     // If joint coordinates were given then use them to decide on node coordinates
@@ -237,169 +237,118 @@ function drawNetwork(coords, edges, elTypes, elNames, threeD=true){
 /* Decide on the coordinates of the nodes given a list of edges
 Edges is a list of lists. Each row is a node and each element in a
 row lists the indexes of the edges shared with the node.
-Algorithm for calculating the coordinates is taken from:
-M J McGuffin, Simple algorithms for network visualisation: A tutorial,
-Tsinghua Science and Technology, 2012. */
-function positionNodes(edges){
-    const L = 10;  // spring rest length
-    const Kr = 100;  // respulsive force constant
-    const Ks = 5;  // spring constant
-    const deltaT = 0.005  // time step
-    
-    const nNodes = edges.length;
-    let coords = [];
-
-    // First, generate coordinates for a cicular layout
-    const jump = 1/nNodes;
-	let theta;
-	for (let i=0; i<nNodes; i++){
-		theta = i * jump * 2 * Math.PI;
-		coords.push([Math.cos(theta), Math.sin(theta), 0])
-    }
-    
-    let i, j, i1, i2, dx, dy, distance, force, fx, fy, distanceSquared;
-
-    // Initialise net forces
-    let forcesX = [];
-    let forcesY = [];
-    for (i=0; i<nNodes; i++){
-        forcesX.push(0)
-        forcesY.push(0)
-    }
-
-    let attempt = 1;
-    //while (checkIntersections(edges, coords)){  // if you want to make sure there are no unwanted intersections
-    while (attempt < 50){
-        // repulsion between all pairs 
-        for (i1=0; i1<nNodes-1; i1++){
-            for (i2=i1+1; i2<nNodes; i2++){
-                dx = coords[i2][0] - coords[i1][0]
-                dy = coords[i2][1] - coords[i1][1]
-                if (dx != 0 || dy != 0){
-                distanceSquared = (dx*dx) + (dy*dy);
-                distance = Math.sqrt(distanceSquared);
-                force = Kr / distanceSquared;
-                fx = force * dx / distance;
-                fy = force * dy / distance;
-                forcesX[i1] -= fx;
-                forcesY[i1] -= fy;
-                forcesX[i2] += fx;
-                forcesY[i2] += fy;
-                }
+Using method detailed in
+    Fruchterman, Thomas MJ, and Edward M. Reingold.
+    "Graph drawing by force‐directed placement."
+    Software: Practice and experience 21, no. 11 (1991): 1129-1164.
+*/
+function fruchterman_reingold(edges, iterations=50, scale=1){
+    function makeArray(w, h, val) {
+        var arr = [];
+        for(let i = 0; i < h; i++) {
+            arr[i] = [];
+            for(let j = 0; j < w; j++) {
+                arr[i][j] = val;
             }
         }
-
-        // sprint force between adjacent pairs
-        for (i1=0; i1<nNodes; i1++){
-            for (j=0; j<edges[i1].length; j++){
-                i2 = edges[i1][j];
-                if (i1 < i2) { // avoid dealing with pairs twice
-                    dx = coords[i2][0] - coords[i1][0]
-                    dy = coords[i2][1] - coords[i1][1]
-                    if (dx != 0 || dy != 0){
-                        distance = Math.sqrt((dx*dx) + (dy*dy));
-                        force = Ks * (distance - L);
-                        fx = force * dx / distance;
-                        fy = force * dy / distance;
-                        forcesX[i1] += fx;  // can you do -= notation here?
-                        forcesY[i1] += fy;
-                        forcesX[i2] -= fx;
-                        forcesY[i2] -= fy;
-                    }
-                }
-            }
-        }
-
-        // update positions
-        for (i=0; i<nNodes; i++){
-            dx = deltaT * forcesX[i];
-            dy = deltaT * forcesY[i];
-            coords[i][0] = coords[i][0] + dx;
-            coords[i][1] = coords[i][1] + dy;
-        }
-    attempt++;
+        return arr;
     }
-    
-    return coords;
-}
+	const dimens = 3;
+	const nVertices = edges.length;
+	const k = Math.sqrt(1 / nVertices);  // Optimal distance between nodes
+	
+	// Create the adjacency matrix
+	let A = makeArray(nVertices, nVertices, 0);
+	let i;
+	for (i=0; i<nVertices; i++){
+		for (const e of edges[i]){
+			A[i][e] = 1;
+			A[e][i] = 1;
+		}
+	}
 
-/* Use this if you want to make sure no edges intersect with a node they shouldn't or with another edge */
-function checkIntersections(edges, coords){
-    const nNodes = edges.length;
-    const radius = 5;
-
-    let minX = 0;
-    let minY = 0
-    let maxX = 0;
-    let maxY = 0;
-    coords.forEach((node) => {
-        minX = Math.min(minX, node[0]);
-        minY = Math.min(minY, node[1]);
-        maxX = Math.max(maxX, node[0]);
-        maxY = Math.max(maxY, node[1]);
-    });
-
-    // Scale coords to fit on the canvas
-    // The canvas is (300 * 150) but scale to (200 * 100) so the nodes aren't right on the edge
-    for (let i=0; i<coords.length; i++){
-        const x = coords[i][0]
-        const y = coords[i][1]
-        coords[i][0] = ((x - minX) / (maxX - minX)) * 200 - 100
-        coords[i][1] = ((y - minY) / (maxY - minY)) * 100 - 50
-    }
-
-    // Check that an edge doesn't intersect with a node not related to the edge
-    let pos1, pos2, j, i1, i2, i3, x, y, dist;
-    let edge_pairs = [];  // list of edges
-    for (i1=0; i1<nNodes; i1++){  // node 1 of edge
-        pos1 = coords[i1];
-        for (j=0; j<edges[i1].length; j++){
-            i2 = edges[i1][j];  // node 2 of edge
-            if (i1 < i2) { // don't calculate for the edge twice
-                pos2 = coords[i2];
-                edge_pairs.push([pos1, pos2]);
-                // Find the equation of the line for the edge
-                const a = pos2[1] - pos1[1];
-                const b = pos1[0] - pos2[0];
-                const c = pos1[1] * (pos2[0] - pos1[0]) - (pos2[1] - pos1[1]) * pos1[0];
-                for (i3=0; i3<nNodes; i3++){  // node 3 tested for intersection
-                    if (i3 != i1 && i3 != i2){  // don't check for the nodes used by the edge
-                        // Check that the edge of nodes i1 and i2 doesn't intersect through node i3
-                        x = coords[i3][0];
-                        y = coords[i3][1];
-                        dist = (Math.abs(a * x + b * y + c)) / 
-                                Math.sqrt(a * a + b * b);
-                        if (radius >= dist){
-                            return true;  // They intersect!
-                        }
-                    }
-                }
-            }
+	// Create random initial positions
+	let pos = [];
+	for (let i = 0; i < nVertices; i++) {
+        pos[i] = [];
+        for (let j = 0; j < dimens; j++) {
+			pos[i][j] = Math.random();
         }
     }
+	
+	// Set initial temperature
+	const arrayColumn = (arr, n) => arr.map(x => x[n]);  // to get a column of an array
+	let t = Math.max((Math.max.apply(Math, arrayColumn(pos, 0)) - Math.min.apply(Math, arrayColumn(pos, 0))),
+	                 (Math.max.apply(Math, arrayColumn(pos, 1)) - Math.min.apply(Math, arrayColumn(pos, 1))));
+	const dt = t / (iterations + 1);
 
-    // Check if two edges intersect
-    // Using maths from https://bryceboe.com/2006/10/23/line-segment-intersection-algorithm/
-    // The two edges are defined by endpoints ab and cd
-    let a, b, c, d;
-    for (i1=0; i1<edge_pairs.length-1; i1++){
-        for (i2=i1+1; i2<edge_pairs.length; i2++){
-            a = edge_pairs[i1][0];
-            b = edge_pairs[i1][1];
-            c = edge_pairs[i2][0];
-            d = edge_pairs[i2][1];
-            if (a != d && b != c){
-                if (ccw(a, c, d) != ccw(b, c, d) && ccw(a, b, c) != ccw(a, b, d) ){
-                    return true;  // They intersect!
-                }
-            }
-        }
-    }
+	let displacement = makeArray(dimens, nVertices, 0);
+	let delta, dist, dx, dy;
+	let length, delta_pos;
+	for (let iter=0; iter<iterations; iter++){
+		// reset displacement
+		for (let i = 0; i < nVertices; i++) {
+			for (let j = 0; j < dimens; j++) {
+				displacement[i][j] = 0;
+			}
+		}
+		
+		for (i=0; i<nVertices; i++){
+			// Get the euclidean distance between this node's position and all others
+			delta = makeArray(2, nVertices, 0);
+			dist = [];
+			for (let j=0; j<nVertices; j++){
+				delta[j][0] = pos[i][0] - pos[j][0];
+				delta[j][1] = pos[i][1] - pos[j][1];
+				dist[j] = Math.sqrt(delta[j][0]**2 + delta[j][1]**2);
+				// Enforce minimum distance of 0.01
+				if (dist[j] < 0.01){
+					dist[j] = 0.01;
+				}
+			}
+			// Get the displacement force and sum for each separate axis
+			dx = 0;
+			dy = 0;
+			for (let j=0; j<nVertices; j++){
+				dx += delta[j][0] * (k * k / dist[j]**2 - A[i][j] * dist[j] / k)
+				dy += delta[j][1] * (k * k / dist[j]**2 - A[i][j] * dist[j] / k)
+			}
+			displacement[i][0] = dx;
+			displacement[i][1] = dy;
+		}
+		
+		// Update positions
+		for (i=0; i<nVertices; i++){
+			length = Math.sqrt(displacement[i][0]**2 + displacement[i][1]**2);
+			if (length < 0.01){
+				length = 0.01;
+			}
+			delta_pos = [displacement[i][0] * t / length, displacement[i][1] * t / length];
+			pos[i][0] += delta_pos[0]
+			pos[i][1] += delta_pos[1]
+		}
 
-    function ccw(a, b, c){
-        return (c[1] - a[1]) * (b[0] - a[0]) > (b[1] - a[1]) * (c[0] - a[0]);
-    }
-    return false;  // No intersections found
+		// Cool temperature
+		t -= dt;
+	}
+
+	// // Rescale
+	let meanX = 0;
+	let meanY = 0;
+	for (i=0; i<nVertices; i++){
+		meanX += pos[i][0];
+		meanY += pos[i][1];
+	}
+	meanX /= nVertices;
+	meanY /= nVertices;
+	for (i=0; i<nVertices; i++){
+		pos[i][0] -= meanX;
+		pos[i][1] -= meanY;
+	}
+	
+	
+	return pos;
+
 }
 
 
